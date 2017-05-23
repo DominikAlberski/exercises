64ff717882e0b4253a9d515e3c86601167ac5007
Dominik Alberski <dominik.alberski@gmail.com>
2017-05-23 17:35:03 +0200

    zadanie plus testy

--- exercise_1.rb
+++ exercise_1.rb
@@ -1,1 +1,13 @@
+require 'minitest/autorun'
+class TestSum < MiniTest::Test
+  def test_sum
+    assert_equal 25, odd_sum(10)
+  end
+  def test_substraction
+    assert_equal 1, odd_sum(1)
+  end
+end
 
+def odd_sum(num)
+  (0..num).inject { |mem, var| var.odd? ? mem += var : mem }
+end
