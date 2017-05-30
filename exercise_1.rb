5041379bb3d4834dec1901f88eac99b27dd6aa98
Dominik Alberski <dominik.alberski@gmail.com>
2017-05-30 18:28:48 +0200

    faster 1 line solution

--- exercise_1.rb
+++ exercise_1.rb
@@ -2,12 +2,26 @@ require 'minitest/autorun'
 class TestSum < MiniTest::Test
   def test_sum
     assert_equal 25, odd_sum(10)
-  end
-  def test_substraction
     assert_equal 1, odd_sum(1)
   end
 end
 
 def odd_sum(num)
-  (0..num).inject { |mem, var| var.odd? ? mem += var : mem }
+  # (0..num).inject { |mem, var| var.odd? ? mem += var : mem }
+  # rozwiązanie 1 linijkowe wydaje się wolniejsze:
+  num.even? ? ((1 + num - 1) / 2 ) * ((num + 1) / 2) : ((1 + num) / 2 ) * ((num + 2) / 2)
+  # num.even? ? num -= 1 : num
+  # ((1 + num) / 2 ) * ((num + 2) / 2)
 end
+
+
+
+def simple_b
+  start = Time.now
+  yield
+  return Time.now - start
+end
+
+puts simple_b { odd_sum(100) }
+puts simple_b { odd_sum(100_000) }
+puts simple_b { odd_sum(100_000_000) }
